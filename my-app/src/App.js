23659020f1fc5106a9bdba014abe3c1bb09c3650
ABCJIIN <wldls0309@naver.com>
2022-04-21 14:28:14 +0900

    0421

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import StartClass from "./components/StartClass";
+import StartFunc from "./components/Startfunc";
+
+function App() {
+  return (
+    <StartClass />,
+    <StartFunc />
+  );
+}
+
+export default App;
---
+++ my-app/src/components/StartFunc.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function StartFunc() {
+    return (
+        <div>
+            <h1>class 컴포넌트</h1>
+            <p>html 적용하기</p>
+        </div>
+    )
+}
+
+export default StartFunc;
